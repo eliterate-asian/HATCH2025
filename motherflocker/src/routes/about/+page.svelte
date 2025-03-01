5ed5c2f317e14806ca509b82f802d0008a0b2c36
eliterate-asian <elise.mcclintock.official@gmail.com>
2025-03-01 13:29:41 -0600

    Scaffolded a simple layout

---
+++ motherflocker/src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	function goTo() {
+		goto('register');
+	}
+</script>
+
+
+<h1>Mother Flocker</h1>
+<h2>Release 0.1</h2>
+<p>A regional database and disease tracker for the risk assessment of avian-flu affected areas.</p>
+<button on:click={goTo}>TAKE ME AWAY</button>
